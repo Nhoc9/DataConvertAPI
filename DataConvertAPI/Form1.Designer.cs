92ab974b0a2c5884b18c9d727190a305b7dccb00
hai.leminh396@gmail.com <hai.leminh396@gmail.com>
2024-05-23 23:41:12 +0700

    ddddd

--- DataConvertAPI/Form1.Designer.cs
+++ DataConvertAPI/Form1.Designer.cs
@@ -52,7 +52,7 @@ namespace DataConvertAPI
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nhập Key 13:";
+            this.label1.Text = "Nhập Key 12:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
