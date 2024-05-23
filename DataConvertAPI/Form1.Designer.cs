beac36c618294d70905ffb0032889648a85eb211
hai.leminh396@gmail.com <hai.leminh396@gmail.com>
2024-05-23 23:22:34 +0700

    fdf

--- DataConvertAPI/Form1.Designer.cs
+++ DataConvertAPI/Form1.Designer.cs
@@ -50,9 +50,9 @@ namespace DataConvertAPI
             this.label1.Font = new System.Drawing.Font(".VnArial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nhập Key 3:";
+            this.label1.Text = "Nhập Key 13:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
